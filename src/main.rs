78e39245dab597e470b8b3f11837382fe61dd184
gcxfd <i@rmw.link>
2021-12-16 16:09:31 +0800

    🔶

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use mdbx::prelude::*;
+use mdbx::r#type::{FromMdbx, ToAsRef};
+use mdbx::{tx::PtrTx, val::MDBX_val, val_bytes};
+use speedy::{Readable, Writable};
+
+#[derive(PartialEq, Debug, Readable, Writable)]
+pub struct City {
+  name: String,
+  lnglat: (u32, u32),
+}
+
+impl FromMdbx for City {
+  fn from_mdbx(_: PtrTx, val: MDBX_val) -> Self {
+    City::read_from_buffer(val_bytes!(val)).unwrap()
+  }
+}
+
+impl ToAsRef<City, Vec<u8>> for City {
+  fn to_as_ref(&self) -> Vec<u8> {
+    self.write_to_vec().unwrap()
+  }
+}
+
+env_rw!(MDBX, {
+  let mut db_path = std::env::current_exe().unwrap();
+  db_path.set_extension("mdb");
+  println!("mdbx file path {}", db_path.display());
+  db_path.into()
+});
+
+mdbx! {
+  MDBX
+  Test
+    key u16
+    val City
+}
+
+fn main() -> Result<()> {
+  let city = City {
+    name: "BeiJing".into(),
+    lnglat: (11640, 3990),
+  };
+
+  let tx = w!();
+  let test = tx | Test;
+  test.set(1, city)?;
+  println!("{:?}", test.get(1)?);
+
+  Ok(())
 }
