ea036cb73007f92b5f6fa3676c421e8b6862a862
gcxfd <i@rmw.link>
2021-12-16 16:11:11 +0800

    🔶

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,27 @@ use speedy::{Readable, Writable};
 
 #[derive(PartialEq, Debug, Readable, Writable)]
 pub struct City {
-    name: String,
-    lnglat: (u32, u32),
+  name: String,
+  lnglat: (u32, u32),
 }
 
 impl FromMdbx for City {
-    fn from_mdbx(_: PtrTx, val: MDBX_val) -> Self {
-        City::read_from_buffer(val_bytes!(val)).unwrap()
-    }
+  fn from_mdbx(_: PtrTx, val: MDBX_val) -> Self {
+    City::read_from_buffer(val_bytes!(val)).unwrap()
+  }
 }
 
 impl ToAsRef<City, Vec<u8>> for City {
-    fn to_as_ref(&self) -> Vec<u8> {
-        self.write_to_vec().unwrap()
-    }
+  fn to_as_ref(&self) -> Vec<u8> {
+    self.write_to_vec().unwrap()
+  }
 }
 
 env_rw!(MDBX, {
-    let mut db_path = std::env::current_exe().unwrap();
-    db_path.set_extension("mdb");
-    println!("mdbx file path {}", db_path.display());
-    db_path.into()
+  let mut db_path = std::env::current_exe().unwrap();
+  db_path.set_extension("mdb");
+  println!("mdbx file path {}", db_path.display());
+  db_path.into()
 });
 
 mdbx! {
@@ -37,15 +37,15 @@ mdbx! {
 }
 
 fn main() -> Result<()> {
-    let city = City {
-        name: "BeiJing".into(),
-        lnglat: (11640, 3990),
-    };
+  let city = City {
+    name: "BeiJing".into(),
+    lnglat: (11640, 3990),
+  };
 
-    let tx = w!();
-    let test = tx | Test;
-    test.set(1, city)?;
-    println!("{:?}", test.get(1)?);
+  let tx = w!();
+  let test = tx | Test;
+  test.set(1, city)?;
+  println!("{:?}", test.get(1)?);
 
-    Ok(())
+  Ok(())
 }
